24171bac97d42f0830424510c089c868dbc848c3
Dmitry-K13 <kudrjavcev.dima@gmail.com>
2024-01-29 21:43:00 +0000

    29.01.

---
+++ 01/main.js
@@ -0,0 +1,9 @@
+let userNickname = 'Alex';
+let userScore = 235;
+
+
+let firstValue = Number(prompt('Цена покупки'));
+let secondValue = Number(prompt('Количество'));
+
+console.log(+firstValue+ +secondValue);
+console.log((+firstValue+ +secondValue)*0.05);
